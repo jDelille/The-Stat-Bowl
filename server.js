00d7e3a18b31ad0600fe0fa06d73f3f2c98f4574
Justin Delille <justind7513@gmail.com>
2021-10-06 14:41:46 -0700

    final project

--- server.js
+++ server.js
@@ -58,9 +58,9 @@ app.get('/match/:home_team_id', async (req, res) => {
 })
 
 
+const port = process.env.PORT || 4000
 
 
 
 
-
-app.listen(4000, () => console.log('listening on port 4000'))
+app.listen(port, () => console.log('listening on port 4000'))
